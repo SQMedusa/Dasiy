760b4451f74155d1f3cf016f1401abafb329ae3e
SQMedusa <2602636912@qq.com>
2020-03-18 21:17:08 +0800

    html+css

---
+++ js/picture.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 26026 on 2019/6/19.
+ */
+pic = $('pic');
+pic.onmouseover = function(){
+    this.style.width = 100 + 'px';
+    this.style.height = 100 +'px';
+}
+pic.onmouseout = function(){
+    this.style.width = 430 + 'px';
+    this.style.height =260 + 'px';
+}
